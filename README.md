ef9278309f8aa271e2e8d56fdebafb9a8b2f9e7a
avlima <albertovieiradelima@gmail.com>
2022-01-13 18:55:04 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# php-cpf-cnpj-generator
-Brazilian document generator
+[![Packagist](https://img.shields.io/packagist/v/avlima/php-cpf-cnpj-generator.svg)](https://packagist.org/packages/avlima/php-cpf-cnpj-gerenator) 
+[![PHP from Packagist](https://img.shields.io/packagist/php-v/avlima/php-cpf-cnpj-generator.svg?logo=php&logoColor=white)](https://packagist.org/packages/avlima/php-cpf-cnpj-generator)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/avlima/php-cpf-cnpj-generator/master/LICENSE)
+
+# Brazilian document generator for PHP
+
+#### [Star ⭐](https://github.com/avlima/php-cpf-cnpj-generator) repo to show suport 😊
+
+## Installation
+
+### Install Package
+Require this package with composer:
+```
+composer require avlima/php-cpf-cnpj-generator
+```
+
+## Usage
+
+Add the class `use Avlima\PhpCpfCnpjGerenator\Generator;` to your file:
+```
+Generator::cpf();
+- output: 84255523037
+
+Generator::cpf(true);
+- output: 842.555.230-37
+
+Generator::cnpj();
+- output: 14905492000179
+
+Generator::cnpj(true);
+- output: 14.905.492/0001-79
+```
+
+
+## Thanks
+
+```
+## Support
+Feel free to post your issues in the issues section.
+
+## Credits
+Developed by [Alberto Lima](https://github.com/avlima "Alberto Vieira de Lima")
+
+## License
+MIT
---
+++ src/Generator.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Avlima\PhpCpfCnpjGerenator;
+
+use Exception;
+
+class Generator
+{
+    /**
+     * @param bool $mask
+     *
+     * @return string
+     * @throws Exception
+     */
+    public static function cnpj(bool $mask = false): string
+    {
+        $n1 = random_int(0, 9);
+        $n2 = random_int(0, 9);
+        $n3 = random_int(0, 9);
+        $n4 = random_int(0, 9);
+        $n5 = random_int(0, 9);
+        $n6 = random_int(0, 9);
+        $n7 = random_int(0, 9);
+        $n8 = random_int(0, 9);
+        $n9 = $n10 = $n11 = 0;
+        $n12 = 1;
+        $d1 = $n12 * 2 + $n11 * 3 + $n10 * 4 + $n9 * 5 + $n8 * 6 + $n7 * 7 + $n6 * 8 + $n5 * 9 + $n4 * 2 + $n3 * 3
+            + $n2 * 4 + $n1 * 5;
+        $d1 = 11 - (self::mod($d1));
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $d2 = $d1 * 2 + $n12 * 3 + $n11 * 4 + $n10 * 5 + $n9 * 6 + $n8 * 7 + $n7 * 8 + $n6 * 9 + $n5 * 2 + $n4 * 3
+            + $n3 * 4 + $n2 * 5 + $n1 * 6;
+        $d2 = 11 - (self::mod($d2));
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        if ($mask === true) {
+            return $n1 . $n2 . "." . $n3 . $n4 . $n5 . "." . $n6 . $n7 . $n8 . "/" . $n9 . $n10 . $n11 . $n12
+                . "-" . $d1 . $d2;
+        }
+
+        return $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $n10 . $n11 . $n12 . $d1 . $d2;
+    }
+
+    /**
+     * @param bool $mask
+     *
+     * @return string
+     * @throws Exception
+     */
+    public static function cpf(bool $mask = false): string
+    {
+        $n1 = random_int(0, 9);
+        $n2 = random_int(0, 9);
+        $n3 = random_int(0, 9);
+        $n4 = random_int(0, 9);
+        $n5 = random_int(0, 9);
+        $n6 = random_int(0, 9);
+        $n7 = random_int(0, 9);
+        $n8 = random_int(0, 9);
+        $n9 = random_int(0, 9);
+        $d1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
+        $d1 = 11 - (self::mod($d1));
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $d2 = $d1 * 2 + $n9 * 3 + $n8 * 4 + $n7 * 5 + $n6 * 6 + $n5 * 7 + $n4 * 8 + $n3 * 9 + $n2 * 10 + $n1 * 11;
+        $d2 = 11 - (self::mod($d2));
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        if ($mask) {
+            return $n1 . $n2 . $n3 . "." . $n4 . $n5 . $n6 . "." . $n7 . $n8 . $n9 . "-" . $d1 . $d2;
+        }
+
+        return $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $d1 . $d2;
+    }
+
+    /**
+     * @param int $dividend
+     *
+     * @return float
+     */
+    private static function mod(int $dividend): float
+    {
+        return round($dividend - (floor($dividend / 11) * 11));
+    }
+}
